a6b1e5cf3322da0721b96a619872210ef70a8d8a
Nikita Kose <nikita@thepixelbot.com>
2024-12-28 15:42:37 +0530

    styling about section done

--- src/main.jsx
+++ src/main.jsx
@@ -14,15 +14,7 @@ ReactDOM.render(
   <BrowserRouter>
    <Header />
       <App />
-      <Footer />
+    <Footer />
   </BrowserRouter>,
   document.getElementById("root")
 );
-
-// createRoot(document.getElementById('root')).render(
-//   <StrictMode>
-//     <Header />
-//       <App />
-//     <Footer />
-//   </StrictMode>
-
--- src/pages/AboutUs.jsx
+++ src/pages/AboutUs.jsx
@@ -4,18 +4,19 @@ import { Container, Row } from 'react-bootstrap'
 export default function AboutUs() {
     return (
         <>
-            <Container className='home-container'>
-               <Row>
-               <div className='text-center py-5 '>
-                    <div className='display-5 py-3 text-start'>
-                    <b className='fs-2'>About StudioWe </b>
-                        believe in fostering creativity and innovation, equipping our students with practical skills to excel in a fast-changing digital world. With expert instructors, real-world projects, and personalized guidance, we ensure our learners are industry-ready from day one.
+            <div className='container my-5'>
+                <div className='row'>
+                    <div className='bg-color p-3 content-height' >
+                        <h1 className='fw-bold py-3'>
+                            ABOUT US
+                        </h1>
+                        <div className='display-5 py-3 lh-base text-start'>
+                            <b className='fs-2'>About StudioWe </b>
+                            believe in fostering creativity and innovation, equipping our students with practical skills to excel in a fast-changing digital world. With expert instructors, real-world projects, and personalized guidance, we ensure our learners are industry-ready from day one.
+                        </div>
                     </div>
-
                 </div>
-               </Row>
-
-            </Container>
+            </div>
         </>
     )
 }
